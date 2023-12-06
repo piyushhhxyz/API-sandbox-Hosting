2988dd17c0b46ad6cd89652310df1f7f9a79457f
piyushhhxyz <piyush.bhawsar1@gmail.com>
2023-12-07 03:22:46 +0530

    fixing..III

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiV1Router = require('./apis/v1');
 const cors = require('cors');
 
-app.use('cors');
+app.use(cors());
 
 const app = express();
 app.use(express.json())
@@ -11,7 +11,7 @@ const port = 3000;
 
 app.use('/github-wrapped/api/v1', apiV1Router);
 
-app.get('/', (req,res) => res.json({msg:'Github-Wrapped By Prassh ka APIs Server'}))
+app.get('/', (_,res) => res.json({msg:'Github-Wrapped By Prassh ka APIs Server'}))
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
